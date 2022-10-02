713e63ad9e04aae8dedd93c6a28bafece2ca2b31
Bryce Haley <bhaley91@gmail.com>
2022-10-02 18:06:58 -0400

    ajax query to fetch tweets from server complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-tweets = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1664555553994
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1664641953994
-  }
-]
-
 const createTweetElement = function(tweet) {
   let $tweet = `<article class="tweet">
   <header>
@@ -61,14 +35,16 @@ const renderTweets = function(tweets) {
   }
 };
 
-renderTweets(tweets);
-
 $(() => { 
   $( "form" ).submit(function( event ) {
     event.preventDefault();
-
     const data = $( this ).serialize();
-    $.ajax('/tweets', { method: 'POST', data})
-
+    $.ajax('/tweets', { method: 'POST', data});
   });
-});
+
+  $(() => 
+    $.ajax('/tweets', { method: 'GET'})
+    .then(function(tweets) {
+      renderTweets(tweets);
+    })
+  )});
